79c0dd68f004ca7b949ef175d66cb10dc6c6b122
Hanszman <victor.hanszman@hotmail.com>
2023-06-14 22:39:52 -0300

    preparations

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,8 +1,16 @@
 // Imports
 import './Home.css';
+// import { useEffect } from 'react';
+// import Api from '../../../services/Api';
 
 // Component
 function Home() {
+    // useEffect(() => {
+    //     Api.get('/').then(res => {
+    //         console.log(res);
+    //     });
+    // }, [])
+
     return (
         <div>
             
