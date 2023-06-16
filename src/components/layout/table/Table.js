18836e7c6a5663a50bf08963f3fd530f2715eefe
Hanszman <victor.hanszman@hotmail.com>
2023-06-16 10:28:40 -0300

    table fixes

--- src/components/layout/table/Table.js
+++ src/components/layout/table/Table.js
@@ -1,44 +1,58 @@
 // Imports
 import './Table.css';
+import { useTranslation } from 'react-i18next';
 
 // Component
 function Table(props) {
+    // Declarations
+    const { t } = useTranslation();
+
     return (
-        <table>
-            <tr>
-                {
-                    props.headers && props.headers.length > 0 && props.headers.map((header) => (
-                        <th key={header}>{header}</th>
-                    ))
-                }
-            </tr>
-            {
-                props.rows && props.rows.length > 0 && props.rows.map((row) => (
-                    <tr
-                        id={row.id}
-                        key={row.id}
-                        title={row.name}
-                    >
+        <div>
+            <table>
+                <thead>
+                    <tr>
                         {
-                            props.data && props.data.length > 0 && props.data.map((data) => (
-                                <td key={data.name}>
-                                    {
-                                        data.type !== 'link' &&
-                                        row[data.name]
-                                    }
-                                    {
-                                        data.type === 'link' &&
-                                        <a href={row[data.name]} target='_blank' rel="noreferrer">
-                                            {row[data.name]}
-                                        </a>
-                                    }
-                                </td>
+                            props.headers && props.headers.length > 0 && props.headers.map((header) => (
+                                <th key={header}>{header}</th>
                             ))
                         }
                     </tr>
-                ))
+                </thead>
+                <tbody>
+                    {
+                        props.rows && props.rows.length > 0 && props.rows.map((row) => (
+                            <tr
+                                id={row.id}
+                                key={row.id}
+                                title={row.name}
+                            >
+                                {
+                                    props.data && props.data.length > 0 && props.data.map((data) => (
+                                        <td key={data.name}>
+                                            {
+                                                data.type !== 'link' &&
+                                                row[data.name]
+                                            }
+                                            {
+                                                data.type === 'link' &&
+                                                <a href={row[data.name]} target='_blank' rel="noreferrer">
+                                                    {row[data.name]}
+                                                </a>
+                                            }
+                                        </td>
+                                    ))
+                                }
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            {
+                !props.rows || props.rows.length <= 0 &&
+                <p>{t('NoData')}</p>
             }
-        </table>
+        </div>
     );
 }
 
